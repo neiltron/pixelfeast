48d2f61c5ed48809f8fe3c50d31e4a3cd48cfdd4
Neil Pullman <npullman@thisisgrow.com>
2017-04-06 16:11:54 -0400

    update camera position on each frame

--- src/camera.js
+++ src/camera.js
@@ -13,6 +13,9 @@ events.keyDown.add(e => {
 
 export default {
   translate(ctx) {
+    x = Math.max(0, Math.min(dimensions.VIEWPORT_WIDTH, Copter.position[0] - Math.floor(dimensions.VIEWPORT_WIDTH / 2)));
+    y = Math.max(0, Math.min(dimensions.VIEWPORT_HEIGHT, Copter.position[1] - Math.floor(dimensions.VIEWPORT_HEIGHT / 2)));
+
     const scale = dimensions.getScale(ctx);
     ctx.translate(-x * scale, -y * scale);
   },
