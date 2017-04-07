d50418be610b31a53b6318cf838f033f58a8735b
Matt Campbell <matt@mattcampbell.net>
2017-04-06 23:35:38 -0400

    Add houses

--- src/assets.js
+++ src/assets.js
@@ -7,6 +7,16 @@ export const images = {
   road1: './static/road1.png',
   road2: './static/road2.png',
   road3: './static/road3.png',
+  house1: './static/house-1.png',
+  house2: './static/house-2.png',
+  house3: './static/house-3.png',
+  house4: './static/house-4.png',
+  house5: './static/house-5.png',
+  house6: './static/house-6.png',
+  house7: './static/house-7.png',
+  house8: './static/house-8.png',
+  house9: './static/house-9.png',
+  house10: './static/house-10.png',
 };
 
 export function loadImages() {
--- src/tiles.js
+++ src/tiles.js
@@ -8,6 +8,7 @@ const TILES = {
   ROAD_H: 1,
   ROAD_V: 2,
   ROAD_INTERSECTION: 3,
+  RESERVED: 4,
 };
 
 const IMAGES = [
@@ -15,6 +16,7 @@ const IMAGES = [
   'road1',
   'road2',
   'road3',
+  'grass',
 ];
 
 const DIRECTIONS = {
@@ -24,7 +26,7 @@ const DIRECTIONS = {
   WEST: [-1, 0],
 };
 
-let tiles;
+let tiles, tileObjects = [];
 
 const generateRoads = () => {
   for (let x = 0; x < dimensions.GRID_WIDTH; x += 1) {
@@ -53,10 +55,59 @@ const generateRoads = () => {
   }
 };
 
+const generateHouses = () => {
+  var count, index, x, y, rowHasHouse = false;
+  for (y = 1; y < dimensions.GRID_HEIGHT; y += 1) {
+    count = 0;
+    rowHasHouse = false;
+    for (x = 0; x < dimensions.GRID_WIDTH; x += 1) {
+      index = y * dimensions.GRID_WIDTH + x;
+      if (tiles[index] === TILES.GRASS) {
+        count += 1;
+      } else {
+        count = 0;
+      }
+
+      if (count < 2) {
+        continue;
+      }
+
+      count = 0;
+
+      if (tiles[(y - 1) * dimensions.GRID_WIDTH + x] !== TILES.GRASS) {
+        continue;
+      }
+
+      if (Math.random() < 0.1) {
+        continue;
+      }
+
+      rowHasHouse = true;
+
+      tiles[index] = TILES.RESERVED;
+      tiles[index - 1] = TILES.RESERVED;
+      tileObjects.push({
+        image: 'house' + rand(1, 10),
+        x: (x - 1) * dimensions.TILE_SIZE,
+        y: (y - 1) * dimensions.TILE_SIZE,
+        width: dimensions.TILE_SIZE * 2,
+        height: dimensions.TILE_SIZE * 2,
+        filter: `hue-rotate(${rand(0, 360)}deg)`,
+      });
+      x += 1;
+    }
+
+    if (rowHasHouse) {
+      y += 1;
+    }
+  }
+};
+
 export default {
   generate() {
     tiles = Array.from(Array(dimensions.GRID_WIDTH * dimensions.GRID_HEIGHT)).map(x => TILES.GRASS);
     generateRoads();
+    generateHouses();
   },
 
   draw(ctx) {
@@ -77,5 +128,20 @@ export default {
         ctx.drawImage(images[IMAGES[tile]], Math.floor(x * dimensions.TILE_SIZE), Math.floor(y * dimensions.TILE_SIZE), dimensions.TILE_SIZE, dimensions.TILE_SIZE);
       }
     }
+
+    ctx.save();
+    tileObjects.forEach(obj => {
+      if (obj.y + obj.height < bounds.y || obj.y > bounds.y + bounds.h) {
+        return;
+      }
+
+      if (obj.x + obj.width < bounds.x || obj.x > bounds.x + bounds.w) {
+        return;
+      }
+
+      ctx.filter = obj.filter;
+      ctx.drawImage(images[obj.image], obj.x, obj.y, obj.width, obj.height);
+    });
+    ctx.restore();
   }
 };
